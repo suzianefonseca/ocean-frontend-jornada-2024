ca430e7c0eb5b1a4250ea451c1b3a9161326a878
suzianefonseca <138014152+suzianefonseca@users.noreply.github.com>
2024-01-25 13:41:47 +0000

    ALTERAÇÃO DO HTML

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <div>
-       <h2>Rick</h2>
+       <h2>Rick Sanchez</h2>
        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="" />
        </div>
       </>
