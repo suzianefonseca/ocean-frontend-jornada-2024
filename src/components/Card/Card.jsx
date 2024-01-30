a582bf3db9be404fbefff80e50cf1ac0542aa415
suzianefonseca <138014152+suzianefonseca@users.noreply.github.com>
2024-01-30 13:36:44 +0000

    configuração para o pronps

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,16 +1,23 @@
-function Card(){
+function Card(props){
+  //console.log(props)
+
+  const item = props.item
+
+  //console.log(item) 
+
     return (
         <>
           <div className='card'>
-           <h2>Rick Sanchez</h2>
+           <h2>{item.name}</h2>
+
               <div className="tags">
                 <div className="tag">Status: Vivo</div>
                 <div className="tag">Espécie: Humana</div>
                 <div className="tag">Origem: Terra c-137</div>
               </div>
 
-                <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="" />
-                </div>
+              <img src={item.image} />
+          </div>
            
           </>
       )
