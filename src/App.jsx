a66acbefceee025819d26e34bd66428d27fe87f6
suzianefonseca <138014152+suzianefonseca@users.noreply.github.com>
2024-01-30 14:17:00 +0000

    estilização dos cards

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,24 @@ const item1 = {
   name: 'Rick Sanchez',
   image:'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
 }
+
 const item2 = {
   name: 'Morty Smith',
   image:'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
 }
+
 const item3 = {
   name: 'Summer smith',
   image:'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
 }
+
   return (
     <>
+    <div className="cards">
       <Card  item={item1} />
       <Card  item={item2}/>
       <Card  item={item3}/>
+    </div>
     </>
   )
 }
