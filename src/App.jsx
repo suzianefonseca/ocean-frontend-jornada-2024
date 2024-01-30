6e3a94a27b06820d5a424fd14868bab612f99503
suzianefonseca <138014152+suzianefonseca@users.noreply.github.com>
2024-01-30 13:44:36 +0000

    adicionando cards

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,19 @@ const item1 = {
   name: 'Rick Sanchez',
   image:'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
 }
-
+const item2 = {
+  name: 'Morty Smith',
+  image:'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+}
+const item3 = {
+  name: 'Summer smith',
+  image:'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+}
   return (
     <>
       <Card  item={item1} />
-      <Card  item={item1}/>
-      
+      <Card  item={item2}/>
+      <Card  item={item3}/>
     </>
   )
 }
