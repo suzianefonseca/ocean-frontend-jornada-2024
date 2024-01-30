4d3a19ac37da3101739b06f316ef0f59b28f2bc5
suzianefonseca <138014152+suzianefonseca@users.noreply.github.com>
2024-01-30 14:43:14 +0000

    lista para os cards

--- src/App.jsx
+++ src/App.jsx
@@ -17,12 +17,20 @@ const item3 = {
   image:'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
 }
 
+const item4 = {
+  name: 'Beth smith',
+  image:'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+}
+
+const itens = [item1, item2, item3, item4 ]
+
   return (
     <>
     <div className="cards">
-      <Card  item={item1} />
+      {/* <Card  item={item1}/>
       <Card  item={item2}/>
-      <Card  item={item3}/>
+      <Card  item={item3}/> */}
+      {itens.map(item => <Card item={item} />)}
     </div>
     </>
   )
