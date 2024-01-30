5cc5df1a96816f6e7dea2ba7eae2d51158ef9328
suzianefonseca <138014152+suzianefonseca@users.noreply.github.com>
2024-01-30 12:54:30 +0000

    elaborando as tags-card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,8 +3,15 @@ function Card(){
         <>
           <div className='card'>
            <h2>Rick Sanchez</h2>
-           <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="" />
-           </div>
+              <div className="tags">
+                <div className="tag">Status: Vivo</div>
+                <div className="tag">Espécie: Humana</div>
+                <div className="tag">Origem: Terra c-137</div>
+              </div>
+
+                <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="" />
+                </div>
+           
           </>
       )
     }
